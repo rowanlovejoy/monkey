1a28b6a393e5543bf566c89d8ecb21b2b8c4a684
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2024-08-05 19:26:16 +0100

    Add token types

--- token/token.go
+++ token/token.go
@@ -6,3 +6,29 @@ type Token struct {
 	Type    TokenType
 	Literal string
 }
+
+const (
+	// Special
+	ILLEGAL = "ILLEGAL" // Unsupported token
+	EOF     = "EOF"     // End of file
+
+	// Identifiers and literals
+	IDENT = "IDENT" // E.g., add, foobar, x, y
+	INT   = "INT"   // E.g., 3, 5
+
+	// Operators
+	ASSIGN = "ASSIGN" // =
+	PLUS   = "PLUS"   // +
+
+	// Delimiters
+	COMMA     = "COMMA"     // ,
+	SEMICOLON = "SEMICOLON" // ;
+	LPAREN    = "LPAREN"    // (
+	RPAREN    = "RPAREN"    // )
+	LBRACE    = "LBRACE"    // {
+	RBRACE    = "RBRACE"    // }
+
+	// Keywords
+	FUNCTION = "FUNCTION" // fn
+	LET      = "LET"      // let
+)
