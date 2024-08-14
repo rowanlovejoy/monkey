c9287403f0acaeab17ee8f4c253df963401db223
Rowan Lovejoy <44379649+rowanlovejoy@users.noreply.github.com>
2024-08-14 20:05:41 +0100

    Add LetStatement and Identifier structs

--- ast/ast.go
+++ ast/ast.go
@@ -1,16 +1,20 @@
 package ast
 
+import "rowanlovejoy/monkey/token"
+
 type Node interface {
 	TokenLiteral() string
 }
 
 type Statement interface {
 	Node
+	// Dummy method to help with type checking
 	statementNode()
 }
 
 type Expression interface {
 	Node
+	// Dummy method to help with type checking
 	expressionNode()
 }
 
@@ -24,4 +28,27 @@ func (p *Program) TokenLiteral() string {
 	} else {
 		return ""
 	}
+}
+
+type LetStatement struct {
+	Token token.Token // token.LET
+	Name  *Identifier // Identifier being bound to
+	Value Expression  // Expression returning the value to be bound
+}
+
+func (ls *LetStatement) statementNode() {}
+
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+type Identifier struct {
+	Token token.Token // token.IDENT
+	Value string      // The identifier literal
+}
+
+func (i *Identifier) expressionNode() {} // Identifiers are expressions because in some cases they *can* produce values, e.g., when binding one variable to another, i.e., let second_identifier = first_identifier;
+
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
 }
